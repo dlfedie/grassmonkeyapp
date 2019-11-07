b1f376417d2e8f4206936df0c8bec3f4389fa9e9
Dustin Fedie <dlfedie@gmail.com>
2019-11-07 15:28:02 -0600

    adds basic form/redirect, etc. just need to get countdown and names in there

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,27 +1,68 @@
 import React, { Component } from 'react';
 
+import monkeyMap from '../images/monkeyMap.jpg';
+import moment from 'moment';
+
 class AboutPage extends Component {
+  state = {
+    timeToGo: 0
+  }
+
+  componentDidMount() {
+    this.setTime();
+  }
+
+  setTime = () => {
+    let clock = moment().add(90, 'seconds').calendar();
+    this.setState({
+      timeToGo: clock
+    })
+  }
+
   render() {
+
+
+
     return (
       <div>
-        <div>
-          <p>
-            Monkey Madness!
-          </p>
-        </div>
+        <h1>
+          Monkey Madness!
+        </h1>
+        <img src={monkeyMap} alt="map" className="monkeyMap" />
+        <h2>
+          (times are approximate..)
+        </h2>
+        <p>
+          6:30-8:00 - Dinner at Ginger Hop
+        </p>
+        <p>
+          8:00 - Party bus pickup!
+        </p>
+        <p>
+          9:00-10:00 - Cedar Inn for Monkey Awards
+        </p>
+        <p>
+          10:30-12:00 - Uptown 
+        </p>
+        <p>
+          12:30-2:00 - The Front
+        </p>
+        <h3>
+          44 Wildcards have joined the party
+        </h3>
+        <h3>
+          1:30 until Graham does something they'll regret
+        </h3>
+        {this.state.timeToGo}
+        <h3>
+          Welcome shot recipe:
+        </h3>
+        <p>
+          Mix 1 part 99 Bananas with 1 Monkey body part. Serve immediately.
+        </p>
       </div>
     )
   }
 }
-
-// const AboutPage = () => (
-//   <div>
-//     <div>
-//       <p>
-//         This about page is for anyone to read!
-//       </p>
-//     </div>
-//   </div>
-// );
 
 export default AboutPage;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,16 +6,16 @@ import './Nav.css';
 
 const Nav = (props) => (
   <div className="nav">
-    <Link to="/home">
-      <h2 className="nav-title">Monkey Bus Tour 2019!</h2>
+    <Link to="/about">
+      <h2 className="nav-title">Grass Monkey Bus Tour 2019!</h2>
     </Link>
     <div className="nav-right">
-      <Link className="nav-link" to="/home">
+      {/* <Link className="nav-link" to="/home"> */}
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
-      </Link>
+        {/* {props.user.id ? 'Home' : 'Login / Register'} */}
+      {/* </Link> */}
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
@@ -26,9 +26,9 @@ const Nav = (props) => (
         </>
       )}
       {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
+      {/* <Link className="nav-link" to="/about">
         About
-      </Link>
+      </Link> */}
     </div>
   </div>
 );
