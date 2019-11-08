550719471f5f3bded680a215feaa13b3a993895a
Dustin Fedie <dlfedie@gmail.com>
2019-11-08 16:51:21 -0600

    adds some font stuff

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -35,44 +35,24 @@ class AboutPage extends Component {
 
     return (
       <div>
-        <h1>
-          Monkey Madness!
-        </h1>
+        <h1>Monkey Madness!</h1>
+        <h2>Monkeys are currently at:</h2>
+        <h2>Home, sleeping</h2>
         <img src={monkeyMap} alt="map" className="monkeyMap" />
-        <h2>
-          (times are approximate..)
-        </h2>
-        <p>
-          6:30-8:00 - Dinner at Ginger Hop
-        </p>
-        <p>
-          8:00 - Party bus pickup!
-        </p>
-        <p>
-          9:00-10:00 - Cedar Inn for Monkey Awards
-        </p>
-        <p>
-          10:30-12:00 - Uptown
-        </p>
-        <p>
-          12:30-2:00 - The Front
-        </p>
-        <h3>
-          44 Wildcards have joined the party
-        </h3>
-        
+        <h2>(times are approximate..)</h2>
+        <p>6:30-8:00 - Dinner at Ginger Hop</p>
+        <p>8:00 - Party bus pickup!</p>
+        <p>9:00-10:00 - Cedar Inn for Monkey Awards</p>
+        <p>10:30-12:00 - Uptown</p>
+        <p>12:30-2:00 - The Front</p>
+        <h3>44 <span className="wildcards">Wildcards</span> have joined the party</h3>
+        <h2>Timer of Regrets</h2>
         <div className="countdownTimer">
           <CountDownTimer />
         </div>
-        <h3>
-          until Graham does something they'll regret
-        </h3>
-        <h3>
-          Welcome shot recipe:
-        </h3>
-        <p>
-          Mix 1 part 99 Bananas with 1 Monkey body part. Serve immediately.
-        </p>
+        <h3>until Graham does something they'll regret</h3>
+        <h3>Welcome shot recipe:</h3>
+        <p>Mix 1 part 99 Bananas with 1 Monkey body part. Serve immediately.</p>
       </div>
     )
   }
